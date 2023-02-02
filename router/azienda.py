e4fea1f9fa45bdebaf119f6a39aa54671b51c61b
Andrei - Stat <andreistatache1997@gmail.com>
2023-02-02 12:33:21 +0100

    response_model_include of router.get fixed

--- router/azienda.py
+++ router/azienda.py
@@ -13,6 +13,6 @@ router = APIRouter(prefix='/azienda', tags=['azienda'])
     '/all',
     response_model=List[Azienda_model],
     response_model_exclude_none=True,
-    response_model_include={'id_azienda', 'nome'})
+    response_model_include={'id_azienda', 'nome', 'p_iva', 'indirizzo', 'cap', 'iban', 'telefono', 'email', 'pec', 'fax'})
 async def get_aziende():
     return Azienda_dao.get_all_companies()
