e27066f55f00d2a3124161b241955aad9e4d6fa2
dev1 - abba <leo.abbatelli@gmail.com>
2023-02-02 16:08:07 +0100

    HTTP

--- router/azienda.py
+++ router/azienda.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status , HTTPException   
 
 from dao.aziendadao import Azienda_dao
 
@@ -16,3 +16,15 @@ router = APIRouter(prefix='/azienda', tags=['azienda'])
     response_model_include={'id_azienda', 'nome', 'p_iva', 'indirizzo', 'cap', 'iban', 'telefono', 'email', 'pec', 'fax'})
 async def get_aziende():
     return Azienda_dao.get_all_companies()
+
+
+@router.get("")
+async def prendi_azienda(azienda_id: str | None=None):
+    if azienda_id:
+        if Azienda_dao.get_all_companies(azienda_id) == None :
+            raise HTTPException (status_code=404, detail="Azienda non trovata")
+        try: 
+            if Azienda_dao.get_all_companies() == []:
+                raise HTTPException (status_code=404, detail="Azienda non trovata")
+        except Exception as e:
+            raise HTTPException (status_code=500, detail=e.msg)
