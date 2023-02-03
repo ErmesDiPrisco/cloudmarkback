031f17b3b6dd0473ee94f6012c91858a6eff5a95
Andrei - Stat <andreistatache1997@gmail.com>
2023-02-03 13:08:47 +0100

    add get_employee_by_id & get_company_by_id methods

--- dao/dipendente_dao.py
+++ dao/dipendente_dao.py
@@ -2,6 +2,8 @@ from dao.utilities.db import Mysql
 from models.dipendente import Dipendente_model
 
 class Dipendente_dao:
+    # READ
+    # all employees
     @classmethod
     def get_all_employees(cls):
         Mysql.openconnection()
@@ -14,6 +16,19 @@ class Dipendente_dao:
                                             email = element[6], telefono = element[7], data_nascita = element[8]))
         Mysql.close_connection()
         return results
+    # employee by id
+    def get_employee_by_id(cls, id):
+        Mysql.openconnection()
+        Mysql.query(f'SELECT * FROM dipendente WHERE id_dipendente = {id}')
+        data = Mysql.get_results()
+        results = list()
+        for element in data:
+            results.append(Dipendente_model(id_dipendente = element[0], nome = element[1], cognome = element[2], 
+                                            cf = element[3], iban = element[4], id_tipo_contratto = element[5], 
+                                            email = element[6], telefono = element[7], data_nascita = element[8]))
+        Mysql.close_connection()
+        return results
+        
     # INSERT
     @classmethod
     def insert_employee(cls, id_dipendente, nome, cognome, cf, iban, id_tipo_contratto, email, telefono, data_nascita):
